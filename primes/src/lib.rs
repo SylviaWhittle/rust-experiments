5fb216e054915a98563e6d7a2f636a62e5ee996a
SylviaWhittle <sylviwhittle@gmail.com>
2023-08-15 23:59:11 +0100

    Start experimenting with ndarray

--- primes/src/lib.rs
+++ primes/src/lib.rs
@@ -3,7 +3,7 @@ pub fn find_primes(max_number: i32) -> i32 {
     let mut latest_prime = 2;
     while i < max_number {
         let mut found_factor = false;
-        for val in 3..(i as f64).sqrt().floor() as i32 + 1 {
+        for val in (3..(i as f64).sqrt().floor() as i32 + 1).step_by(2) {
             if i % val == 0 {
                 found_factor = true;
                 break;

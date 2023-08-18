9997b4f48b8d5c2a1c2acb94c47429c4b7b196bf
SylviaWhittle <sylviwhittle@gmail.com>
2023-08-18 01:19:13 +0100

    Improve error handling and implement renaming functionality

--- bulk_rename/src/main.rs
+++ bulk_rename/src/main.rs
@@ -2,24 +2,26 @@ use std::env;
 use std::fs;
 use std::path::Path;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
     println!("arguments: {:?}", args);
 
     let relative_path = &args[1];
-
-    if let Ok(current_dir) = env::current_dir() {
-        println!("current directory: {:?}", current_dir);
-
-        let directory_path = current_dir.join(relative_path);
-
-        if let Ok(entries) = fs::read_dir(directory_path) {
-            for entry in entries {
-                println!("{:?}", entry);
-            }
-        }
-    } else {
-        println!("error retrieving current directory");
+    let current_dir = env::current_dir()?;
+    println!("current directory: {:?}", current_dir);
+    let directory_path = current_dir.join(relative_path);
+    let entries = fs::read_dir(directory_path)?;
+    for (i, entry_result) in entries.enumerate() {
+        let entry = entry_result?;
+        let file_path = entry.path();
+        println!("{:?}", file_path);
+        let parent_dir = file_path
+            .parent()
+            .expect("Parent directory of file not found!");
+        let new_file_name = format!("renamed_{}.png", i);
+        fs::rename(&file_path, parent_dir.join(new_file_name))?;
     }
+
+    Ok(())
 }

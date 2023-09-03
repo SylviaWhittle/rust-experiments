fe605850b0267ab278c4652c4c419c4807c314e6
SylviaWhittle <sylviwhittle@gmail.com>
2023-09-03 12:13:59 +0100

    Add saving result directly in rust!

--- stochastic-web/src/main.rs
+++ stochastic-web/src/main.rs
@@ -1,4 +1,5 @@
 use byteorder::{LittleEndian, WriteBytesExt};
+use image::{Rgb, RgbImage};
 use plotters::prelude::*;
 use std::f64::consts;
 use std::fs;
@@ -7,6 +8,38 @@ use std::path::Path;
 
 const OUT_FILE_NAME: &'static str = "./image.png";
 const SCALING: u32 = 1000;
+
+fn filter_vectors(
+    vec1: Vec<f64>,
+    vec2: Vec<f64>,
+    lower_lim: f64,
+    upper_lim: f64,
+) -> (Vec<f64>, Vec<f64>) {
+    let mut res1 = Vec::new();
+    let mut res2 = Vec::new();
+    for (v1, v2) in vec1.into_iter().zip(vec2.into_iter()) {
+        if v1.abs() >= lower_lim / 2.0
+            && v1.abs() < upper_lim / 2.0
+            && v2.abs() >= lower_lim / 2.0
+            && v2.abs() < upper_lim / 2.0
+        {
+            res1.push(v1 + upper_lim / 2.0);
+            res2.push(v2 + upper_lim / 2.0);
+        }
+    }
+    (res1, res2)
+}
+
+fn convert_to_u32(vec: Vec<f64>, multiplier: u32) -> Vec<u32> {
+    vec.into_iter()
+        .map(|x| {
+            if x < 0.0 {
+                panic!("Negative value detected!")
+            }
+            (x * multiplier as f64).round() as u32
+        })
+        .collect()
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Initial conditions
@@ -24,7 +57,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let tau = 2.0 * consts::PI * r / q;
 
     // End time
-    let end_time = 80000000;
+    let end_time = 10000000;
 
     // Create arrays
     let mut x: Vec<f64> = vec![0.0; end_time];
@@ -84,6 +117,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // println!("x: {:?}", x);
         // println!("p: {:?}", p);
     }
+
+    // a default (black) image containing Rgb values
+    let img_size = 5000;
+    let mut img = RgbImage::new(img_size + 1, img_size + 1);
+    let mapping_maximum = 8.0;
+    // println!("x: {:?}", x);
+    println!("len x: {:?}", x.len());
+    // let filtered_x = filter_vector(x, 0.0, mapping_maximum);
+    // let filtered_p = filter_vector(p, 0.0, mapping_maximum);
+    let (filtered_x, filtered_p) = filter_vectors(x, p, 0.0, mapping_maximum);
+    // println!("filtered x: {:?}", filtered_x);
+    println!("len filtered x: {:?}", filtered_x.len());
+    // println!("filtered p: {:?}", filtered_p);
+    let multiplier = img_size / mapping_maximum as u32;
+    let filtered_x = convert_to_u32(filtered_x, multiplier);
+    let filtered_p = convert_to_u32(filtered_p, multiplier);
+    // println!("integer filtered x: {:?}", filtered_x);
+    // println!("integer filtered p: {:?}", filtered_p);
+    for (val_x, val_p) in filtered_x.iter().zip(filtered_p.iter()) {
+        img.put_pixel(*val_x, *val_p, image::Rgb([255, 255, 255]));
+    }
+
+    img.save(Path::new("./output.png"))?;
 
     Ok(())
 

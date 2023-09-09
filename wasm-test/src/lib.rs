30d6509fc29e7dd9a29220f588b46dfad367227b
SylviaWhittle <sylviwhittle@gmail.com>
2023-09-09 16:28:01 +0100

    Add wasm project: random points renderer

--- wasm-test/src/lib.rs
+++ wasm-test/src/lib.rs
@@ -1,33 +1,34 @@
+use rand::Rng;
 use wasm_bindgen::prelude::*;
-use web_sys::HtmlCanvasElement;
-
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
+use wasm_bindgen::JsCast;
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
-    // Set up canvas
-    let window = web_sys::window().unwrap();
-    let document = window.document().unwrap();
-    let body = document.body().unwrap();
-    let canvas = document
-        .create_element("canvas")?
-        .dyn_into::<HtmlCanvasElement>()?;
-    canvas.set_width(400);
-    canvas.set_height(400);
-    body.append_child(&canvas)?;
-
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas: HtmlCanvasElement = canvas.dyn_into::<HtmlCanvasElement>()?;
     let context = canvas
         .get_context("2d")?
         .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
+        .dyn_into::<CanvasRenderingContext2d>()?;
 
-    // Draw a square
-    context.set_fill_style(&JsValue::from_str("blue"));
-    context.fill_rect(100.0, 100.0, 200.0, 200.0);
+    draw_random_points(&context);
 
     Ok(())
+}
+
+fn draw_random_points(context: &CanvasRenderingContext2d) {
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..100 {
+        let x: f64 = rng.gen_range(0.0..800.0);
+        let y: f64 = rng.gen_range(0.0..800.0);
+
+        context.begin_path();
+        context
+            .arc(x, y, 2.0, 0.0, std::f64::consts::PI * 2.0)
+            .unwrap();
+        context.fill();
+    }
 }
